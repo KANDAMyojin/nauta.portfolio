0190a652f709522d92d5355ad62386a0c778179f
k4nd4 <double.k.0508@gmail.com>
2021-04-29 09:46:13 +0900

    アイコンをカードのフッターに表示

--- src/components/member.tsx
+++ src/components/member.tsx
@@ -3,67 +3,64 @@ import { FC } from "react"
 import { GraphCms_Member } from "../generated/graphql"
 import { graphql, useStaticQuery } from "gatsby"
 
-const Member: FC<{member: GraphCms_Member}> = ({member}) => {
+const Member: FC<{ member: GraphCms_Member }> = ({ member }) => {
 
   const data = useStaticQuery(graphql`
-    query {
-        github: imageSharp(resize: {originalName: {eq: "GitHub-Mark-32px.png"}}) {
-            resize {
-                originalName
-                src
-            }
-        }
+      query {
+          github: imageSharp(resize: {originalName: {eq: "GitHub-Mark-120px-plus.png"}}) {
+              resize {
+                  originalName
+                  src
+              }
+          }
 
-        twitter: imageSharp(resize: {originalName: {eq: "Twitter social icons - square - blue.png"}}) {
-            resize {
-                originalName
-                src
-            }
-        }
-    }
+          twitter: imageSharp(resize: {originalName: {eq: "Twitter social icons - square - blue.png"}}) {
+              resize {
+                  originalName
+                  src
+              }
+          }
+      }
   `)
 
   const gitHubImage = data.github.resize
   const twitterImage = data.twitter.resize
 
-  return(
+  return (
     <div className="card">
       <div className="card-content">
         <div className="media">
           <div className="media-left">
             <figure className="image is-96x96">
-              <img src={!!member.icon ? member.icon.url : `https://bulma.io/images/placeholders/96x96.png`} alt="Placeholder image" />
+              <img src={!!member.icon ? member.icon.url : `https://bulma.io/images/placeholders/96x96.png`}
+                   alt="Placeholder image" />
             </figure>
           </div>
           <div className="media-content">
-            <p className="title is-4">{member.name}</p>
-            <div className="tags has-addons">
-              <span className="tag">
-                <figure className="image is-16x16">
-                  <img src={gitHubImage.src} />
-                </figure>
-              </span>
-              <span className="tag is-gray">{`@${member.githubAccount}`}</span>
-            </div><div className="tags has-addons">
-              <span className="tag">
-                <figure className="image is-16x16">
-                  <img src={twitterImage.src} />
-                </figure>
-              </span>
-            <span className="tag is-gray">{`@${member.twitterAccount}`}</span>
+            <p className="title">{member.name}</p>
+            <p className="subtitle">{member.technicalFields}</p>
           </div>
-          </div>
+
         </div>
-
-        <div className="content">
-          {member.description}
-        </div>
-
-        <footer className="card-footer">
-          <a className="card-footer-item" href={`/member/${member.name}`}>Show More</a>
-        </footer>
-
       </div>
+
+      <div className="content">
+        {member.description}
+      </div>
+
+      <footer className="card-footer level">
+          <div className="level-item">
+            <figure className="image is-32x32">
+              <img src={gitHubImage.src} />
+            </figure>
+          </div>
+          <div className="level-item">
+            <figure className="image is-32x32">
+              <img src={twitterImage.src} />
+            </figure>
+          </div>
+        <a className="card-footer-item" href={`/member/${member.name}`}>Show More</a>
+      </footer>
     </div>
   )
 }
